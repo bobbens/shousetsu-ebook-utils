f8f8952c2bf573bbfd8b4027bd93a660354cc747
Edgar Simo-Serra <bobbens@gmail.com>
2012-02-12 17:21:20 +0100

    * More scraping.

--- scrape.sh
+++ scrape.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 INURLS=( http://ncode.syosetu.com/n8725k/
-         http://ncode.syosetu.com/n8709j/ )
+         http://ncode.syosetu.com/n8709j/
+         http://ncode.syosetu.com/n5174n/
+         http://ncode.syosetu.com/n6366u/
+         http://ncode.syosetu.com/n6963w/ 
+         http://ncode.syosetu.com/n0126r/ 
+         http://ncode.syosetu.com/n8462u/ )
 
 for x in "${INURLS[@]}"
 do
